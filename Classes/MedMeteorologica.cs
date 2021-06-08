e3206de86cc301c386fbad3013c5479296c20a41
lucas <llucas.andrads@gmail.com>
2021-06-08 08:24:46 -0300

    Projeto inicial a partir da aula 1

---
+++ Classes/MedMeteorologica.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes {
+
+    //atributo
+    public class MedidaMeteorologica {
+        public double temperatura;
+        public double umidade;
+
+        public double Temperatura {
+            get {
+                return temperatura;
+            }
+            set {
+                if (value >= -273.15) {
+                    temperatura = value;
+                }
+                else {
+                    Console.WriteLine("Não existe temperatura inferior ao zero absoluto");
+                }
+            }
+        }
+        public double Umidade {
+            get {
+                return umidade;
+            }
+            set {
+                if (value <= 100 && value >= 0) {
+                    umidade = value;
+                }
+                else {
+                    Console.WriteLine("A umidade deve estar entre 0 e 100%");
+                }
+            }
+        }
+        public double TemperaturaFahrenheit {
+            get {
+                return 1.8 * temperatura + 32;
+            }
+            set {
+                Temperatura = (value - 32) / 1.8;
+            }
+        }
+        // Métodos
+        public string ImprimaValores(string nome) {
+            string saida = "";
+
+            saida = "Medida " + nome + ": temperatura = " + Temperatura + ", temp. em Fahrenheit = "
+                + TemperaturaFahrenheit + ", umidade = " + Umidade;
+
+            return saida;
+        }
+        // Construtores
+        public MedidaMeteorologica(double Temperatura, double Umidade) {
+            this.Temperatura = Temperatura;
+            this.Umidade = Umidade;
+        }
+        public MedidaMeteorologica() {
+            // Construtor sem parâmetros, não é o contrutor padrão!!!
+            Temperatura = 20;
+            Umidade = 60;
+        }
+    }
+}
---
+++ TesteClasse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes;
+
+//Oficial -- 
+namespace TesteClasse {
+    class Program {
+        static void Main(string[] args) {
+            MedidaMeteorologica met1 = new MedidaMeteorologica();
+            met1.Temperatura = 25;
+            met1.Umidade = 60;
+            Console.WriteLine(met1.ImprimaValores("met1"));
+
+            MedidaMeteorologica met2 = new MedidaMeteorologica();
+            met2.Temperatura = -500;
+            met2.Umidade = 150;
+            Console.WriteLine(met2.ImprimaValores("met2"));
+
+            MedidaMeteorologica met3 = new MedidaMeteorologica(35, 20);
+            Console.WriteLine(met3.ImprimaValores("met3"));
+
+            MedidaMeteorologica met4 = new MedidaMeteorologica();
+            met4.TemperaturaFahrenheit = 95;
+            met4.Umidade = 15;
+            Console.WriteLine(met4.ImprimaValores("met4"));
+
+
+            Console.ReadKey();
+        }
+    }
+}
